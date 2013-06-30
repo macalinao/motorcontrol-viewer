48d8e14fdb04c54e9595b34dbf5c9e16c4a32e60
Ian Macalinao <ianmacalinao@gmail.com>
2013-06-29 20:10:35 -0500

    Add graphs
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ app.config(function($routeProvider) {
     when('/graphs', {
       templateUrl: './partials/graphs.html',
       controller: function GraphsCtrl($scope) {
-        $scope.sets = sets;
+        $scope.graph = {
+          title: {
+            text: 'Data Set Graphs'
+          },
+          xAxis: {
+            title: {
+              text: 'Time'
+            }
+          },
+          legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            borderWidth: 0
+          },
+          credits: {
+            enabled: false
+          },
+          series: sets
+        };
       }
     }).
 
@@ -80,4 +99,40 @@ app.directive('fileUpload', function($parse) {
         });
     }
   }
+});
+
+// Chart directive https://github.com/rootux/angular-highcharts-directive/
+app.directive('chart', function () {
+  return {
+    restrict: 'E',
+    template: '<div></div>',
+    scope: {
+        chartData: "=value"
+    },
+    transclude:true,
+    replace: true,
+
+    link: function (scope, element, attrs) {
+      var chartsDefaults = {
+        chart: {
+          renderTo: element[0],
+          type: attrs.type || null,
+          height: attrs.height || null,
+          width: attrs.width || null
+        }
+      };
+      
+      //Update when charts data changes
+      scope.$watch(function() { return scope.chartData; }, function(value) {
+        if(!value) return;
+          // We need deep copy in order to NOT override original chart object.
+          // This allows us to override chart data member and still the keep
+          // our original renderTo will be the same
+          var deepCopy = true;
+          var newSettings = {};
+          $.extend(deepCopy, newSettings, chartsDefaults, scope.chartData);
+          var chart = new Highcharts.Chart(newSettings);
+      });
+    }
+  };
 });
